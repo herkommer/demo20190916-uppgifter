a9ce38521df9b7c815b625dcbfcf47cb0219948a
herkommer <m@herkommer.co.uk>
2019-09-16 09:34:16 +0200

    Uppgift 1.1 done

--- Uppgifter/Uppgift 1/Program.cs
+++ Uppgifter/Uppgift 1/Program.cs
@@ -10,6 +10,16 @@ namespace Uppgift_1
     {
         static void Main(string[] args)
         {
+            //          Låt användaren mata in en mening.
+            Console.WriteLine("Please enter a line of text:");
+            string userresponse;
+            userresponse = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you for entering: '{0}'", userresponse);
+
+//          Skriv ut antal ord
+//          Skriv ut varje ord på en egen rad med antal tecken i ordet(ex hej(3))
         }
     }
 }
