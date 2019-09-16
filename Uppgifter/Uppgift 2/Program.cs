8402e1e136513f046ba04eeff5a685e302cecbf8
herkommer <m@herkommer.co.uk>
2019-09-16 10:14:14 +0200

    Preparing for Uppgift 2

---
+++ Uppgifter/Uppgift 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //          Låt användaren mata in en mening.
+            Console.WriteLine("Please enter a sentence:");
+            string userresponse = Console.ReadLine();
+
+//          Skriv ut varje ord på en egen rad, i storleksordning(längd på ordet)
+
+//          Skriv ut varje ord på en egen rad, i omvänd ordning(baklänges)
+        }
+    }
+}
