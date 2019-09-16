f0e88f435e7b586612ba036252801dd41d00f559
herkommer <m@herkommer.co.uk>
2019-09-16 10:31:54 +0200

    Uppgift 2.1 progressing, but needs refactoring

--- Uppgifter/Uppgift 2/Program.cs
+++ Uppgifter/Uppgift 2/Program.cs
@@ -14,7 +14,47 @@ namespace Uppgift_2
             Console.WriteLine("Please enter a sentence:");
             string userresponse = Console.ReadLine();
 
-//          Skriv ut varje ord på en egen rad, i storleksordning(längd på ordet)
+            //          Skriv ut varje ord på en egen rad, i storleksordning(längd på ordet)
+            //Tanke: behandla strängen som en array av char
+            //när vi hittar ett space, bryt raden
+            //hmm, detta tar inte hänsyn till storleken...
+            //vi behöver få en array av ord!
+
+            //Hitta antalet ord
+            int wordcount = 1; //dvs när ett space hittas är det ord 2
+
+            foreach (char c in userresponse)
+            {
+                if (c == ' ')
+                {
+                    wordcount++;
+                }
+            }
+
+            string[] words = new string[wordcount]; //hmm, how big? antal spaces!
+            string word = "";
+            int i = 0;
+            foreach (char c in userresponse)
+            {
+                if (c != ' ')
+                { 
+                    word = word + c;
+                }
+                else
+                {
+                    words[i] = word;
+                    word = "";
+                    i++;
+                }
+            }
+
+            Console.WriteLine("Words består av {0} ord", words.Length);
+            Console.WriteLine(words);
+
+            foreach (string s in words)
+            {
+                Console.WriteLine("{0} ({1})",s, s.Length);
+            }
 
 //          Skriv ut varje ord på en egen rad, i omvänd ordning(baklänges)
         }
